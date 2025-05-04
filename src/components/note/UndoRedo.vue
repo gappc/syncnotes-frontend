0ae3ae99af38cf7c82ffa7b756e2f013fe12e7dd
gappc <gapp.christian@gmail.com>
2025-05-04 18:53:17 +0200

    Refactor UndoRedo component to use SVG icons for undo and redo actions

--- src/components/note/UndoRedo.vue
+++ src/components/note/UndoRedo.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="flex gap-2">
-    <ButtonCustom @click="undoManager.undo()">undo</ButtonCustom>
-    <ButtonCustom @click="undoManager.redo()">redo</ButtonCustom>
+    <ButtonCustom @click="undoManager.undo()">
+      <IconArrowGoBack class="size-5" />
+    </ButtonCustom>
+    <ButtonCustom @click="undoManager.redo()">
+      <IconArrowGoForward class="size-5" />
+    </ButtonCustom>
   </div>
 </template>
 
 <script setup lang="ts">
 import ButtonCustom from '@/components/button/ButtonCustom.vue'
 import type { UndoManager } from 'yjs'
+import IconArrowGoBack from '../svg/IconArrowGoBack.vue'
+import IconArrowGoForward from '../svg/IconArrowGoForward.vue'
 
 defineProps<{ undoManager: UndoManager }>()
 </script>
